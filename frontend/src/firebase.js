47282d72437297837a4ebef6b2d035f7621ffe2e
vlsharshav <vlsharshav@gmail.com>
2026-04-07 12:20:54 +0530

    Clear Firebase configuration values
    Removed sensitive Firebase configuration details.

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -3,13 +3,13 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBbsdcMyCLinymMjrp5EYAotqsQElhvnP0",
-  authDomain: "dabs-40c16.firebaseapp.com",
-  projectId: "dabs-40c16",
-  storageBucket: "dabs-40c16.firebasestorage.app",
-  messagingSenderId: "823419398543",
-  appId: "1:823419398543:web:54a05b1d1f932b11a643fa",
-  measurementId: "G-6EJZF5WXNF"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
